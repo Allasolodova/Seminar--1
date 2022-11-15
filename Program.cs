1ce1fee0bd7547c93b42855082282b0f9dbf6606
Alla <molotovalana@mail.ru>
2022-11-16 01:38:22 +0300

    Задачи

---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿//однострочный комментарий
+/*ghjksjhgvvbdhvdkvdhjvdhvdhvdkhjvdjhvdhjjjdhvdvhdsvh
+dgdgdjgekjghleiulgyhwjghrghrghge
+desgdfghdfhgjhgfjfgjgf*/
+
+
+//ctrl+/
+
+//Напишитепрограмму которая на вход принимает2 числа и проверяет,является ли первое число кважратом  второго
+/*
+Console. WriteLine ("Введи первое число");
+int Num1 = int. Parse (Console.ReadLine ()!);
+Console. WriteLine ("Введи второе число");
+int Num2 = int. Parse (Console.ReadLine ()!);
+
+if(Math.Pow(Num2, 2)== Num1){
+     Console. WriteLine ("Первое число является квадратом второго !");
+}
+else{
+     Console. WriteLine ("Первое число не является квадратом второго !");
+}
+*/
+/* 
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine ()!);
+
+int b = a*a;
+
+Console.WriteLine("Квадрат заданного числа -" + b);  */
+
+//задача 6
+
+/*
+Console.WriteLine("Введите число");
+int i = int.Parse(Console.ReadLine()!);
+
+if(i%2 ==0){
+
+     Console.WriteLine("Введённое число является чётным");
+}
+else{
+
+     Console.WriteLine("Введённое число является нечётным");
+}*/
+
+//Задача № 8
+/*
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+
+while(i<= N)
+
+if(i %2 ==0)
+{
+     Console.WriteLine($"{i}, ");
+
+     i++;
+}
+else{
+
+     i++;
+}*/
+//Задача 2
+/*
+Console.WriteLine("Введите прервое число");
+int Num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int Num2 = int.Parse(Console.ReadLine()!);
+ master
+if(Num1<Num2){
+
+     Console.WriteLine("Первое число меньше второго числа");
+}
+else{
+
+     Console.WriteLine("Первое число больше второго числа");
+}*/
+
+//Задача 4
+/*
+Console.WriteLine("введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine()!);
+
+if(a>b){
+
+     Console.WriteLine("Первое число максимальное");
+}
+else{
+
+     Console.WriteLine("второе число максимальное");
+}
+
+if(b>c){
+
+     Console.WriteLine("Второе число максимальное");
+
+}
+else{
+
+     Console.WriteLine("Третье число максимальное");
+}
+
+if(c>a){
+
+     Console.WriteLine("Третье число максимальное");
+}
+
+else{
+
+     Console.WriteLine("Первое число максимальное");
+}
+*/
+
+
+
+
+
+
+
+
+
